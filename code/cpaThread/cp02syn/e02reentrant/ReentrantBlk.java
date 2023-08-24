03c630453a657364643afb5c3949ade331212f81
JasonNerd <duan912rainbow@qq.com>
2023-08-24 23:38:30 +0800

    优化了代码结构

--- code/cpaThread/cp02syn/e02reentrant/ReentrantBlk.java
+++ code/cpaThread/cp02syn/e02reentrant/ReentrantBlk.java
@@ -1,11 +1,11 @@
-package cpaThread.conAccess.exm;
+package cpaThread.cp02syn.exm;
 
 public class ReentrantBlk {
     private int n;
     public synchronized void setA(int n) throws InterruptedException {
         System.out.println(Thread.currentThread().getName()+" in updateLongTime() is going to update shared-var as "+n);
         this.n = n;
-        Thread.sleep((int)(Math.random()*1200));    // 写入变量后随机等待较长的时间(在这段时间内, 可能有其他线程进行了更改)
+        Thread.sleep((int)(Math.random()*1200));    // 写入变量后随机等待较长的时间(进行了同步, this.n 不会有更改)
         get();      // get() 也由 synchronized 修饰, 执行其中的代码需要获取对象锁
     }
 
